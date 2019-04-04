077425df3059a1d48d2e4f70424eb10ec19a3076
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 17:00:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,8 +11,13 @@ def second_challenge
       }
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => {
+      }
+      :matriarch => {
+      }
+      :heroine => {
+      }
+:heroine_friends => []
+      }
+      }
 end

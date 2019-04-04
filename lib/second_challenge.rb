3a561a3af7c09581b0f7f4f3596cd00797193a3f
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 17:06:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,22 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-      }
-      :matriarch => {
-      }
-      :hero => {
-      }
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      }, 
+    }, 
     :capulet => {
-      :patriarch => {
-      }
-      :matriarch => {
-      }
-      :heroine => {
-      }
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
 :heroine_friends => []
-      }}
+    }
+  }
   
 end
